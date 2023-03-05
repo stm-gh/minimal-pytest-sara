0aeb4c5291bff3e9814344e57be67d65eea7ed46
Sara_TM <stmsara1@gmail.com>
2023-03-05 11:15:15 -0800

    Added square decorator; tests passing

--- pwgen/add.py
+++ pwgen/add.py
@@ -1,2 +1,14 @@
+def squareDecorator(func):
+    def wrapper(*args, **kwargs):
+        print("Start of wrap")
+        result = func(*args, **kwargs)*func(*args, **kwargs)
+        ##result = func + func
+        print(result)
+        return result
+    return wrapper
+
+@squareDecorator
 def add(a, b):
     return a + b
+
+print("End of add.py")
--- pwgen/tests/test_add.py
+++ pwgen/tests/test_add.py
@@ -1,4 +1,4 @@
 from pwgen import add
 
 def test_df():
-    assert add.add(1, 2) == 3
+    assert add.add(1, 2) == 9
